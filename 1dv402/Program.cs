a29dd9d09e101238f0e7ccb5f1eaeadb4aa922a8
rasmusir <israelssonrasmusraz@gmail.com>
2014-09-03 23:50:22 +0200

    Added ReadUint(string prompt, double min)

--- 1dv402/Program.cs
+++ 1dv402/Program.cs
@@ -10,14 +10,15 @@ namespace _1dv402
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ReadPositiveDouble("Ange totalsumma\t\t: "));
+            double total = ReadPositiveDouble("Ange totalsumma\t\t: ");
+            uint cash = ReadUint("Ange erhållet belopp\t: ", total);
+
         }
 
         static double ReadPositiveDouble(string prompt = null)
         {
-            bool reading = true;
             double value = 0;
-            while (reading)
+            while (true)
             {
                 if (prompt != null)
                 {
@@ -29,22 +30,55 @@ namespace _1dv402
                 if (Double.TryParse(Console.ReadLine().Replace('.',','), out value))
                 {
                     double roundedValue = Math.Round(value, MidpointRounding.AwayFromZero);
-                    if (roundedValue>0)
-                    {
-                        reading = false;
-                    }
+                    if (roundedValue > 0)
+                        break;
                     else
                     {
-                        Console.WriteLine("Whops! '{0}' är en för liten summa.",value);
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Whops! '{0}' är en för liten summa.", value);
+                        Console.ResetColor();
                     }
                 }
                 else
                 {
+                    Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("Whops! Du har inte skrivit in en giltig summa.");
+                    Console.ResetColor();
                 }
             }
             return value;
 
         }
+
+        static uint ReadUint(string prompt, double min)
+        {
+            uint value = 0;
+
+            while (true)
+            {
+                Console.Write(prompt);
+
+                if (UInt32.TryParse(Console.ReadLine(), out value))
+                {
+                    if (value < min)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Whops! {0} är ett för litet belopp.", value);
+                        Console.ResetColor();
+                    }
+                    else
+                        break;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Whops! Du har inte skrivit in en giltig summa.");
+                    Console.ResetColor();
+                }
+            }
+
+            return value;
+        }
+
     }
 }

da873ecb9147014bce4faebefacc97858a1b1573
rasmusir <israelssonrasmusraz@gmail.com>
2014-09-03 15:48:03 +0200

    Implemented ReadPositiveDouble([prompt])

--- 1dv402/Program.cs
+++ 1dv402/Program.cs
@@ -10,6 +10,41 @@ namespace _1dv402
     {
         static void Main(string[] args)
         {
+            ReadPositiveDouble("Ange totalsumma\t\t: ");
+        }
+
+        static double ReadPositiveDouble(string prompt = null)
+        {
+            bool reading = true;
+            double value = 0;
+            while (reading)
+            {
+                if (prompt != null)
+                {
+                    Console.Write(prompt);
+                }
+
+                value = 0;
+
+                if (Double.TryParse(Console.ReadLine(), out value))
+                {
+                    double roundedValue = Math.Round(value, MidpointRounding.AwayFromZero);
+                    if (roundedValue>0)
+                    {
+                        reading = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Whops! '{0}' är en för liten summa.",value);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Whops! Du har inte skrivit in en giltig summa.");
+                }
+            }
+            return value;
+
         }
     }
 }
